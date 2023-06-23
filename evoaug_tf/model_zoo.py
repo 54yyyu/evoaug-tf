22456d53aff98cb98c446df806ec8a91a16d60a7
54yyyu <2322740290@qq.com>
2023-06-23 11:13:04 -0400

    update

--- evoaug_tf/model_zoo.py
+++ evoaug_tf/model_zoo.py
@@ -2,58 +2,6 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-def DeepSTARR(input_shape):
-
-    # body
-    inputs = keras.layers.Input(shape=input_shape)
-    x = keras.layers.Conv1D(256, kernel_size=7, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(inputs)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Activation('relu')(x)
-    #x = keras.layers.LeakyReLU()(x)
-    x = keras.layers.MaxPooling1D(2)(x)
-    x = keras.layers.Dropout(0.2)(x)
-
-    x = keras.layers.Conv1D(60, kernel_size=3, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(x)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Activation('relu')(x)
-    #x = keras.layers.LeakyReLU()(x)
-    x = keras.layers.MaxPooling1D(2)(x)
-    x = keras.layers.Dropout(0.2)(x)
-
-    x = keras.layers.Conv1D(60, kernel_size=5, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(x)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Activation('relu')(x)
-    #x = keras.layers.LeakyReLU()(x)
-    x = keras.layers.MaxPooling1D(2)(x)
-    x = keras.layers.Dropout(0.2)(x)
-
-    x = keras.layers.Conv1D(120, kernel_size=3, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(x)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Activation('relu')(x)
-    #x = keras.layers.LeakyReLU()(x)
-    x = keras.layers.MaxPooling1D(2)(x)
-    x = keras.layers.Dropout(0.2)(x)
-
-    x = keras.layers.Flatten()(x)
-
-    x = keras.layers.Dense(256)(x)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Activation('relu')(x)
-    #x = keras.layers.LeakyReLU()(x)
-    x = keras.layers.Dropout(0.4)(x)
-
-    x = keras.layers.Dense(256)(x)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Activation('relu')(x)
-    #x = keras.layers.LeakyReLU()(x)
-    x = keras.layers.Dropout(0.4)(x)
-
-    outputs = keras.layers.Dense(2, activation='linear')(x)
-
-    return inputs, outputs
-
-
-
 params = {'kernel_size1': 7,
           'kernel_size2': 3,
           'kernel_size3': 5,
@@ -105,5 +53,57 @@ def DeepSTARR_ori(input_shape, params=params):
     
     # heads per task (developmental and housekeeping enhancer activities)
     outputs = keras.layers.Dense(2, activation='linear')(x) #tasks = ['Dev', 'Hk']
+
+    return inputs, outputs
+
+
+
+def DeepSTARR_v1(input_shape):
+
+    # body
+    inputs = keras.layers.Input(shape=input_shape)
+    x = keras.layers.Conv1D(256, kernel_size=7, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(inputs)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Activation('relu')(x)
+    #x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.MaxPooling1D(2)(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Conv1D(60, kernel_size=3, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Activation('relu')(x)
+    #x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.MaxPooling1D(2)(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Conv1D(60, kernel_size=5, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Activation('relu')(x)
+    #x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.MaxPooling1D(2)(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Conv1D(120, kernel_size=3, padding='same', kernel_initializer=tf.keras.initializers.HeNormal())(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Activation('relu')(x)
+    #x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.MaxPooling1D(2)(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Flatten()(x)
+
+    x = keras.layers.Dense(256)(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Activation('relu')(x)
+    #x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.Dropout(0.4)(x)
+
+    x = keras.layers.Dense(256)(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Activation('relu')(x)
+    #x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.Dropout(0.4)(x)
+
+    outputs = keras.layers.Dense(2, activation='linear')(x)
 
     return inputs, outputs
