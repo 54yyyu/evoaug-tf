993fc955be0b0ac985f537af1ef3663cfb29964b
54yyyu <2322740290@qq.com>
2023-06-24 15:13:35 -0400

    update

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -9,10 +9,10 @@ from tensorflow import keras
 from evoaug_tf import evoaug_tf, augment, model_zoo
 from evoaug_tf.utils import pearson_r, Spearman, H5DataLoader, evaluate_model
 
-group_name = "final_check"
+group_name = "analysis"
 
 filepath = '../datasets/deepstarr_data.h5'
-x_train, y_train, x_valid, y_valid, x_test, y_test = H5DataLoader(filepath, downsample=20000)
+x_train, y_train, x_valid, y_valid, x_test, y_test = H5DataLoader(filepath)
 _, L, A = x_valid.shape
 
 output_dir = 'result/'
@@ -21,9 +21,9 @@ if not os.path.exists(output_dir):
 
 augment_lists = [
     augment.RandomDeletion(delete_min=0, delete_max=30),
-    augment.RandomRC(rc_prob=0.5), 
-    augment.RandomInsertion(insert_min=0, insert_max=20), 
-    augment.RandomTranslocation(shift_min=0, shift_max=20), 
+    augment.RandomRC(rc_prob=0.5),
+    augment.RandomInsertion(insert_min=0, insert_max=20),
+    augment.RandomTranslocation(shift_min=0, shift_max=20),
     augment.RandomNoise(noise_mean=0, noise_std=0.3),
     augment.RandomMutation(mutate_frac=0.05)
 ]
@@ -33,17 +33,17 @@ num_trials = 5
 
 for aug_index in range(len(augment_lists)):
     augment_list = [augment_lists[aug_index]]
-    exp_name = exp_name_list[aug_index]
-    
+    expt_name = exp_name_list[aug_index]
+
     trial_aug_results = []
     trial_finetune_results = []
     for trial in range(num_trials):
-        exp_name = exp_name+"_"+str(trial)
-        
+        exp_name = expt_name+"_"+str(trial)
+
         keras.backend.clear_session()
 
         batch_size = 100
-        epochs = 5
+        epochs = 100
         finetune_epochs = 5
 
         try:
@@ -52,16 +52,16 @@ for aug_index in range(len(augment_lists)):
             pass
         wandb.init(project="evoaug", group=group_name, name=exp_name)
 
-        inputs, outputs = DeepSTARR_ori(input_shape=(L+evoaug_tf.augment_max_len(augment_list),A))#, tasks=['Dev','Hk'])
-        model = rmodel.RobustModel(inputs=inputs, outputs=outputs, augment_list=augment_list, max_augs_per_seq=2, hard_aug=True)
+        inputs, outputs = model_zoo.DeepSTARR(input_shape=(L+evoaug_tf.augment_max_len(augment_list),A))#, tasks=['Dev','Hk'])
+        model = evoaug_tf.RobustModel(inputs=inputs, outputs=outputs, augment_list=augment_list, max_augs_per_seq=2, hard_aug=True)
 
         model.compile(keras.optimizers.Adam(learning_rate=0.001, decay=1e-6), #weight_decay
                     loss='mse', #  ['mse','mse']
                     #loss_weights=[1, 1], # loss weigths to balance
                     metrics=[Spearman, pearson_r]) # additional track metric
-        
+
         # early stopping callback
-        es_callback = keras.callbacks.EarlyStopping(monitor='val_loss', #'val_aupr',#
+        es_callback = keras.callbacks.EarlyStopping(monitor='val_loss',
                                                     patience=10,
                                                     verbose=1,
                                                     mode='min',
@@ -105,11 +105,8 @@ for aug_index in range(len(augment_lists)):
 
         # ----------------- Fine Tune Analysis -----------------
 
-        finetune_optimizer = keras.optimizers.Adam(learning_rate=0.0001, weight_decay=1e-6)
+        finetune_optimizer = keras.optimizers.Adam(learning_rate=0.0001, decay=1e-6)
         model.finetune_mode(optimizer=finetune_optimizer)
-
-        pred = model.predict(x_test, batch_size=512)
-        finetune_results = evaluate_model(y_test, pred)
 
         ckpt_path = os.path.join(output_dir, exp_name+"_finetune.h5")
         checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path,
@@ -124,7 +121,7 @@ for aug_index in range(len(augment_lists)):
                         batch_size=batch_size,
                         shuffle=True,
                         validation_data=(x_valid, y_valid),
-                        callbacks=[checkpoint, WandbCallback()])
+                        callbacks=[checkpoint]) #WandbCallback()
 
         model.load_weights(ckpt_path)
 
@@ -145,6 +142,6 @@ for aug_index in range(len(augment_lists)):
 
         wandb.finish()
 
-    with open(os.path.join(output_dir, exp_name+'_analysis.pickle'), 'wb') as fout:
+    with open(os.path.join(output_dir, expt_name+'_analysis.pickle'), 'wb') as fout:
         cPickle.dump(trial_finetune_results, fout)
         cPickle.dump(trial_aug_results, fout)
